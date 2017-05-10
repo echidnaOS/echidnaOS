d42423c869bae5b57948700bcf25910cc5f045fb
Mintz98 <the.mintlander@gmail.com>
2017-05-10 08:07:22 +0200

    modified io, added new libc

--- kernel/syscalls/io.c
+++ kernel/syscalls/io.c
@@ -9,11 +9,15 @@ void char_to_stdout(int c) {
 
 // gets a char from the current standard input
 // for now, it will get the char from the keyboard buffer
-// returns 0 if no input
+// returns -2 (NO_INPUT) if no input
 // returns char on input
+// returns EOF on EOF (not implemented yet)
 // DOES NOT HALT EXECUTION UNTIL INPUT
+#define NO_INPUT -2
+
 int char_from_stdin(void) {
     int c;
-    c = keyboard_fetch_char();
+    if ((c=keyboard_fetch_char())=='\0')
+        c = NO_INPUT;
     return c;
 }

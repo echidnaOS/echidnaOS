af4fef3be1cdccfe676661007ffae5bbc58ac5f2
Mintz98 <the.mintlander@gmail.com>
2017-05-18 11:38:06 +0200

    fixed pic

---
+++ kernel/asm/i686/real.asm
@@ -0,0 +1,55 @@
+extern map_PIC
+
+global real_routine
+
+section .data
+
+%define real_init_size  real_init_end - real_init
+real_init:              incbin "kernel/asm/i686/real_init.bin"
+real_init_end:
+
+section .text
+
+bits 32
+
+real_routine:
+    ; EBX, EDX = routine's arguments
+    ; ESI = routine location
+    ; ECX = routine size
+    
+    push esi
+    push ecx
+    
+    ; Disable ints
+    cli
+    
+    ; Remap PIC for a real mode environment
+    push 0x00000070
+    push 0x00000008
+    call map_PIC
+    add esp, 8
+
+    ; Real mode init blob to 0000:1000
+    mov esi, real_init
+    mov edi, 0x1000
+    mov ecx, real_init_size
+    rep movsb
+    
+    ; Routine's blob to 0000:8000
+    pop ecx
+    pop esi
+    mov edi, 0x8000
+    rep movsb
+    
+    ; Call module
+    call 0x1000
+    
+    ; Remap PIC for a pmode environment
+    push 0x00000028
+    push 0x00000020
+    call map_PIC
+    add esp, 8
+    
+    ; Reenable ints and return
+    sti
+    ret
--- kernel/asm/i686/video.asm
+++ kernel/asm/i686/video.asm
@@ -1,12 +1,10 @@
+extern real_routine
+
 ;global vga_set_mode
 ;global vga_disable_cursor
 global real_test
 
 section .data
-
-%define real_init_size real_init_end - real_init
-real_init:          incbin "kernel/asm/i686/real_init.bin"
-real_init_end:
 
 %define test_size   test_end - test
 test:               incbin "kernel/asm/i686/test.bin"
@@ -17,18 +15,8 @@ section .text
 bits 32
 
 real_test:
-    ; Real mode init blob to 0000:1000
-    mov esi, real_init
-    mov edi, 0x1000
-    mov ecx, real_init_size
-    rep movsb
-    
-    ; Test blob to 0000:8000
+xchg bx, bx
     mov esi, test
-    mov edi, 0x8000
     mov ecx, test_size
-    rep movsb
-    
-    ; Call module
-    call 0x08:0x1000
+    call real_routine
     ret
